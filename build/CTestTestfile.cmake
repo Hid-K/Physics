6bc0979c911e30466ea25f2107f3301c081f3568
Laptev Alexander <hidknzk@gmail.com>
2020-12-03 19:44:56 +0300

    deleted build/

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/laptevaleksander/Documents/Projects/Physics
-# Build directory: /Users/laptevaleksander/Documents/Projects/Physics/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
